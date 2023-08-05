36ce6bb55c5f7f3bfbcc5679ff2792ba1e6bb20f
jjortiz016 <jjortiz016@misena.edu.co>
2023-08-04 19:20:46 -0500

    se creo los metodos save,delete, findById en la interfaz IClienteService y ClienteServiceImple

--- src/main/java/com/orbitecsl/springbootbackendapirest/models/services/ClienteServiceImple.java
+++ src/main/java/com/orbitecsl/springbootbackendapirest/models/services/ClienteServiceImple.java
@@ -17,4 +17,23 @@ public class ClienteServiceImple implements IClienteService{
     public List<Cliente> findAll() {
         return (List <Cliente>) iRepositoryClienteDao.findAll();
     }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente findById(Long id) {
+        return iRepositoryClienteDao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return iRepositoryClienteDao.save(cliente);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        iRepositoryClienteDao.findById(id);
+
+    }
 }
