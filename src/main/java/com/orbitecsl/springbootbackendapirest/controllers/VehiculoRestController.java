ad4976a596e8ba5b904216fef2859691e9f1c6f1
jjortiz016 <jjortiz016@misena.edu.co>
2023-10-28 19:05:57 -0500

    se agrego la linea  para el manejo de CORS en el vehiculoRestController  @CrossOrigin.

--- src/main/java/com/orbitecsl/springbootbackendapirest/controllers/VehiculoRestController.java
+++ src/main/java/com/orbitecsl/springbootbackendapirest/controllers/VehiculoRestController.java
@@ -3,12 +3,13 @@ package com.orbitecsl.springbootbackendapirest.controllers;
 import com.orbitecsl.springbootbackendapirest.models.entity.Vehiculo;
 import com.orbitecsl.springbootbackendapirest.models.services.IVehiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins={"http://localhost:4200/"})
 @RestController
 @RequestMapping("/api2")
 public class VehiculoRestController {
