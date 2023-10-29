77016fb59a3169e1b6a304111fabf32f5a98b680
jjortiz016 <jjortiz016@misena.edu.co>
2023-10-28 21:02:07 -0500

    Se creo metodos create, update, delete, y show(consultar por id) en el controlador.

--- src/main/java/com/orbitecsl/springbootbackendapirest/controllers/VehiculoRestController.java
+++ src/main/java/com/orbitecsl/springbootbackendapirest/controllers/VehiculoRestController.java
@@ -3,6 +3,7 @@ package com.orbitecsl.springbootbackendapirest.controllers;
 import com.orbitecsl.springbootbackendapirest.models.entity.Vehiculo;
 import com.orbitecsl.springbootbackendapirest.models.services.IVehiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,9 +19,32 @@ public class VehiculoRestController {
         return iVehiculoService.findAll();
     }
 
-    @GetMapping("/clientes/{id)")
-    public Vehiculo show (){
-        return null;
+    @GetMapping("/vehiculos/{id}")
+    public Vehiculo show (@PathVariable Long id){
+        return iVehiculoService.findById(id);
     }
+
+    @PostMapping("/vehiculos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Vehiculo create(@RequestBody Vehiculo vehiculo){
+        return iVehiculoService.save(vehiculo);
+    }
+
+    @PutMapping("/vehiculos/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Vehiculo update(@RequestBody Vehiculo vehiculo, @PathVariable Long id){
+        Vehiculo vehiculoActual= iVehiculoService.findById(id);
+        vehiculoActual.setPlaca(vehiculo.getPlaca());
+        vehiculoActual.setMarca(vehiculo.getMarca());
+        vehiculoActual.setColor(vehiculo.getColor());
+        vehiculoActual.setTipo(vehiculo.getTipo());
+        return iVehiculoService.save(vehiculoActual);
+    }
+    @DeleteMapping("/vehiculos/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        iVehiculoService.delete(id);
+    }
+
 
 }
