446702e600266476d37f96f32dd22c1b582c347f
jjortiz016 <jjortiz016@misena.edu.co>
2023-11-05 20:47:29 -0500

    Se creo el manejo del error en el metodos show, validando errores de conexion a la base de datos y si existe el id del vehiculo consultado.

--- src/main/java/com/orbitecsl/springbootbackendapirest/controllers/VehiculoRestController.java
+++ src/main/java/com/orbitecsl/springbootbackendapirest/controllers/VehiculoRestController.java
@@ -3,10 +3,15 @@ package com.orbitecsl.springbootbackendapirest.controllers;
 import com.orbitecsl.springbootbackendapirest.models.entity.Vehiculo;
 import com.orbitecsl.springbootbackendapirest.models.services.IVehiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @CrossOrigin(origins={"http://localhost:4200/"})
 @RestController
 @RequestMapping("/api2")
@@ -20,13 +25,31 @@ public class VehiculoRestController {
     }
 
     @GetMapping("/vehiculos/{id}")
-    public Vehiculo show (@PathVariable Long id){
-        return iVehiculoService.findById(id);
+    public ResponseEntity<?> show (@PathVariable Long id){
+        Vehiculo vehiculo=null;
+        Map<String, Object> response= new HashMap<>();
+        try {
+            vehiculo= iVehiculoService.findById(id);
+        }catch (DataAccessException e){
+            response.put("mensaje", "Error al realizar la consulta en la base de datos.");
+            response.put("error", e.getMessage().concat(" :").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if(vehiculo==null){
+            response.put("mensaje","El vehiculo ID:".concat(id.toString().concat(" no existe en la base de datos")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Vehiculo>(vehiculo, HttpStatus.OK);
     }
 
     @PostMapping("/vehiculos")
     @ResponseStatus(HttpStatus.CREATED)
     public Vehiculo create(@RequestBody Vehiculo vehiculo){
+
+
+
         return iVehiculoService.save(vehiculo);
     }
 
