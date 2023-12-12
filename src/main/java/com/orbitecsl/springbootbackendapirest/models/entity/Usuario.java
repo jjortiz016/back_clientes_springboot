4d527810c24fa1420129dca2fc22877a7d577811
jjortiz016 <jjortiz016@misena.edu.co>
2023-12-12 16:36:35 -0500

    Se agregaron las dependencias necesarias OAuth2 for spring security, spring security JWT Library,JAXB API para las versiones superiores a java 8, JAXB Runtime para la seguriddad y se crearon las entidades Usuario y roles.

---
+++ src/main/java/com/orbitecsl/springbootbackendapirest/models/entity/Usuario.java
@@ -0,0 +1,68 @@
+package com.orbitecsl.springbootbackendapirest.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name="usuarios")
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (unique=true, length=20)
+    private String username;
+
+    @Column (length = 80)
+    private String password;
+
+    private Boolean enabled;
+
+    @ManyToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL) //CascadeType.All para cuando elimine el usuario se elimine los roles relacionados, o cuando se creo el usuairo se cree el role
+    //si queremso cambiar el nombre de la tabla intermedia
+    @JoinTable(name="users_authorities", joinColumns = @JoinColumn(name="user_id"), inverseJoinColumns=@JoinColumn(name="role_id"))
+    private List<Role> roles;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
