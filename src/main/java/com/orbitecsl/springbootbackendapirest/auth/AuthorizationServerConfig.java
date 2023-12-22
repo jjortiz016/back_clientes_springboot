13d8bef2e93ef354dabfa83f89ddeb20177fe735
jjortiz016 <jjortiz016@misena.edu.co>
2023-12-22 11:28:42 -0500

    Añadiendo configuración de las aplicaciones clientes y accesoa endpoints

--- src/main/java/com/orbitecsl/springbootbackendapirest/auth/AuthorizationServerConfig.java
+++ src/main/java/com/orbitecsl/springbootbackendapirest/auth/AuthorizationServerConfig.java
@@ -5,15 +5,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -27,17 +25,25 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     @Qualifier("authenticationManager")
     private AuthenticationManager authenticationManager;
-    //lo segundo es implementar 3 metodos de configuracion
+
+
+       //lo segundo es implementar 3 metodos de configuracion
 
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        super.configure(security);
+       security.tokenKeyAccess("permitAll()") //damos permiso para dar acceso a cualquier usuario anonimo o no
+               .checkTokenAccess("isAuthenticated"); //valida el token que se esta enviando
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        super.configure(clients);
+      clients.inMemory().withClient("angularapp")
+              .secret(passwordEncoder.encode("12345"))
+              .scopes("read","write") //permiso que va a tener la aplicacion le damos permiso lectura y escritorio
+              .authorizedGrantTypes("password", "refresh_token")//tipo de autenticación, otro es el refresh_token permite optener un token de acceso renovado
+              .accessTokenValiditySeconds(3600) //valides en cuanto tiempo va a caducar el token en segundos
+              .refreshTokenValiditySeconds(3600);// tiempo de expiracion del refresh token
     }
 
     @Override
